fafbfc426d21441b3fc21d208d3b1d89c2edbc47
alirfanyasin <risayunita21@gmail.com>
2023-06-08 14:05:50 +0700

    operator aritmatika

---
+++ learn-basic-dart/bin/05_arithmetic_operator/main.dart
@@ -0,0 +1,20 @@
+void main() {
+  /// Operator Aritmatika
+  /*
+    +   : Penjumlahan
+    -   : Pengurangan atau negatif
+    *   : Perkalian
+    /   : Pembagian, hasil double
+    ~/  : Pembagian, hasil int
+    %   : Modulus atau sisa bagi
+  */
+
+  num numb1 = 10, numb2 = 3;
+
+  num pemjumlahan = numb1 + numb2;
+  num pengurangan = numb1 - numb2;
+  num perkalian = numb1 * numb2;
+  num pembagian1 = numb1 / numb2;
+  num pembagian2 = numb1 ~/ numb2;
+  num modulus = numb1 % numb2;
+}
