02671b9ea3bfd803dcef9eb203006a86faff12cb
alirfanyasin <risayunita21@gmail.com>
2023-06-08 13:34:11 +0700

    type data dynamic

---
+++ learn-basic-dart/bin/03_type_data/dynamic.dart
@@ -0,0 +1,17 @@
+void main() {
+  /// Dynamic
+  /*
+    Type data dynamic bisa menampung semua jenis type data
+  */
+  dynamic variabel = 100; // int
+  print(variabel);
+
+  variabel = 10.5; // double
+  print(variabel);
+
+  variabel = 'Irfan Yasin'; // string
+  print(variabel);
+
+  variabel = true; // boolean
+  print(variabel);
+}
