328a0b5c1ce8eded8940a340791e8233d767e98c
alirfanyasin <risayunita21@gmail.com>
2023-06-08 13:02:21 +0700

    type data number

---
+++ learn-basic-dart/bin/03_type_data/number.dart
@@ -0,0 +1,15 @@
+void main() {
+  // int hanya bisa menampung bilangan bulat
+  int number1 = 10;
+
+  // double hanya bisa menampung bilangan desimal
+  double number2 = 10.5;
+
+  // num bisa menampung bilangan bulat dan desimal
+  num angka = 12;
+  angka = 12.5;
+
+  print(number1);
+  print(number2);
+  print(angka);
+}
