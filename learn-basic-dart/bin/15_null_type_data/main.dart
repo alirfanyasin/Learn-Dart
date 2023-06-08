b771281c82a7f6edb7c1f5e8254f848c8217da06
alirfanyasin <risayunita21@gmail.com>
2023-06-08 20:34:45 +0700

    null

---
+++ learn-basic-dart/bin/15_null_type_data/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  /// Null
+  /*
+    Kata kunci null merupakan type data representasi data kosong.
+    Secara default jika membuat variabel namun tidak mendeklarasikan value, maka datanya null
+    Namun jika kita ingin membuat sebuah variabel bisa diisi dengan null, dengan menambahkan karakter ? (tanda tanya)
+  */
+
+  int number1; // error
+  int? number2; // null
+
+  int angka1 = null; // error
+  int? angka2 = null; // null
+}
