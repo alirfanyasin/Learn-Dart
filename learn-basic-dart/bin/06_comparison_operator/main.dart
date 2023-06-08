df1b7a4025e9d4966f3f38c594aa04f22c45bdd7
alirfanyasin <risayunita21@gmail.com>
2023-06-08 14:15:08 +0700

    opertator perbandingan

---
+++ learn-basic-dart/bin/06_comparison_operator/main.dart
@@ -0,0 +1,24 @@
+void main() {
+  /// Operator Perbandingan
+  /*
+    Operator perbandingan adalah operator yang hasilnya adalah boolean
+    Operator perbandingan bisa digunakan di tipe data Number dan String
+
+    ==    : Sama dengan
+    !=    : Tidak sama dengan
+    >     : Lebih dari
+    <     : Kurang dari
+    <=    : Lebih dari atau sama dengan
+    >=    : Kurang dari atau sama dengan
+  */
+
+  print(10 == 10); // true
+  print(10 != 10); // false
+  print(10 > 10); // false
+  print(10 < 10); // false
+  print(10 <= 10); // true
+  print(10 >= 10); // true
+
+  print("Irfan" == "Irfan"); // true
+  print("Irfan" != "Irfan"); // false
+}
