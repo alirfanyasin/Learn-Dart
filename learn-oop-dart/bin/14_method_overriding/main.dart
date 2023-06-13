fdd52e1e85f7043328d0c339660ba6045ce6822a
alirfanyasin <risayunita21@gmail.com>
2023-06-14 04:28:18 +0700

    method overrding

---
+++ learn-oop-dart/bin/14_method_overriding/main.dart
@@ -0,0 +1,34 @@
+class Manager {
+  String? name;
+
+  void sayHello(String name) {
+    print('Hello $name, my name is Manager ${this.name}');
+  }
+}
+
+class VicePresident extends Manager {
+  // Overriding
+  void sayHello(String name) {
+    print('Hello $name, my name is VP ${this.name}');
+  }
+}
+
+void main() {
+  /// Method Overriding
+  /* 
+    Method overriding adalah kemampuan mendeklarasikan ulang method di child class,
+    yang sudah ada di parent class.
+    Saat kita melakukan proses overriding tersebut, secara otomatis ketika kita membuat
+    object dari class child, method yang di class parent tidak bisa di akses lagi.
+    Saat melakukan proses overriding, kita harus pastikan deklarasi method yang harus 
+    sama dengan di parent classnya.
+  */
+
+  var manager = Manager();
+  print(manager.name = 'Dilna Azizah');
+  manager.sayHello('Irfan Yasin');
+
+  var vp = VicePresident();
+  print(vp.name = 'Rian Afandi');
+  vp.sayHello('Saipul Kilab');
+}
