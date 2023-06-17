e1e6d729166f04bc35d3a095f1f5bc5100ec20f2
alirfanyasin <risayunita21@gmail.com>
2023-06-18 04:46:13 +0700

    equals operator

---
+++ learn-oop-dart/bin/29_equals_operator/main.dart
@@ -0,0 +1,25 @@
+class Category {
+  String? id;
+  String? name;
+
+  Category(this.id, this.name);
+}
+
+void main() {
+  /// Equals Operator
+  /*
+    Untuk membandingkan dua buah object apakah sama atau tidak, biasanya kit amenggunakan 
+    opertator ==
+    Secara default operator == adalah milik class object, implementasinya jika ingin menggunakan
+    operator == milik class Object adalah akan melakukan pengecekan kesamaan object dari lokasi
+    di memory.
+    Kadang-kadang, kita ingin mengimplementasikan logika untuk membandingkan object.
+    Untuk hal ini kita bisa melakukan override operator == yang ada di class object.
+  */
+
+  var category1 = Category("1", "Laptop");
+  var category2 = Category("1", "Laptop");
+
+  print(category1 == category1); // true
+  print(category1 == category2); // false
+}
